0b6164a9cd3c1100694f3ba128cb8423febfe059
Hrishikesh Huilgolkar <hrishikesh911@gmail.com>
2019-08-11 01:49:27 +0530

    more helper functions more stakemanager functions

--- test/StakeManager.js
+++ test/StakeManager.js
@@ -4,7 +4,7 @@
 // // TODO:
 // // test same vote values, stakes
 // test penalizeEpochs
-// const { assertRevert } = require('./helpers/assertRevert')
+const { assertRevert } = require('./helpers/assertRevert')
 // let functions = require('./helpers/functions')
 // let BlockManager = artifacts.require('./BlockManager.sol')
 let StakeManager = artifacts.require('./StakeManager.sol')
@@ -25,6 +25,7 @@ contract('StakeManager', function (accounts) {
   contract('SimpleToken', async function () {
     // let blockManager = await BlockManager.deployed()
     // let voteManager = await VoteManager.deployed()
+    // let stakeManager = await StakeManager.deployed()
 
     it('shuld be able to initialize', async function () {
       // let stakeManager = await StakeManager.deployed()
@@ -32,16 +33,16 @@ contract('StakeManager', function (accounts) {
       let sch = await SimpleToken.deployed()
       await stateManager.setEpoch(1)
       await stateManager.setState(0)
-      await sch.transfer(accounts[1], 420000, { 'from': accounts[0] })
-      await sch.transfer(accounts[2], 19000, { 'from': accounts[0]})
-      await sch.transfer(accounts[3], 800000, { 'from': accounts[0]})
-      await sch.transfer(accounts[4], 600000, { 'from': accounts[0]})
-      await sch.transfer(accounts[5], 2000, { 'from': accounts[0]})
-      await sch.transfer(accounts[6], 700000, { 'from': accounts[0]})
-      await sch.transfer(accounts[7], 3000, { 'from': accounts[0]})
-      await sch.transfer(accounts[8], 4000, { 'from': accounts[0]})
-      await sch.transfer(accounts[9], 5000, { 'from': accounts[0]})
-      await sch.transfer(accounts[10], 6000, { 'from': accounts[0]})
+      await sch.transfer(accounts[1], 423000, { 'from': accounts[0] })
+      await sch.transfer(accounts[2], 19000, { 'from': accounts[0] })
+      // await sch.transfer(accounts[3], 800000, { 'from': accounts[0]})
+      // await sch.transfer(accounts[4], 600000, { 'from': accounts[0]})
+      // await sch.transfer(accounts[5], 2000, { 'from': accounts[0]})
+      // await sch.transfer(accounts[6], 700000, { 'from': accounts[0]})
+      // await sch.transfer(accounts[7], 3000, { 'from': accounts[0]})
+      // await sch.transfer(accounts[8], 4000, { 'from': accounts[0]})
+      // await sch.transfer(accounts[9], 5000, { 'from': accounts[0]})
+      // await sch.transfer(accounts[10], 6000, { 'from': accounts[0]})
     })
 
     it('should be able to stake', async function () {
@@ -66,6 +67,22 @@ contract('StakeManager', function (accounts) {
       // assert(totalStake.toString() === '420000')
     })
 
+    it('should handle second staker correctly', async function () {
+      let sch = await SimpleToken.deployed()
+      let stakeManager = await StakeManager.deployed()
+
+      await sch.approve(stakeManager.address, 19000, { 'from': accounts[2] })
+      await stakeManager.stake(1, 19000, { 'from': accounts[2] })
+
+      let stakerId = await stakeManager.stakerIds(accounts[2])
+      assert(stakerId.toString() === '2')
+      let numStakers = await stakeManager.numStakers()
+      assert(numStakers.toString() === '2')
+      let staker = await stakeManager.stakers(2)
+      assert(staker.id.toString() === '2')
+      assert(staker.stake.toString() === '19000')
+    })
+
     it('getters should work as expected', async function () {
       // console.log(web3i.eth.accounts)
       let stakeManager = await StakeManager.deployed()
@@ -87,6 +104,51 @@ contract('StakeManager', function (accounts) {
       assert(staker.stake.toString() === String(staker2.stake))
       // let totalStake = await stakeManager.totalStake()
       // assert(totalStake.toString() === '420000')
+    })
+
+    it('should be able to increase stake', async function () {
+      let stakeManager = await StakeManager.deployed()
+      let sch = await SimpleToken.deployed()
+      await sch.approve(stakeManager.address, 3000, { 'from': accounts[1] })
+      await stakeManager.stake(1, 3000, { 'from': accounts[1] })
+      let staker = await stakeManager.getStaker(1)
+      assert(Number(staker.stake) === 423000)
+    })
+
+    it('should not be able to unstake before unstake lock period', async function () {
+      let stakeManager = await StakeManager.deployed()
+      await assertRevert(stakeManager.unstake(1, { 'from': accounts[1] }))
+      // let staker = await stakeManager.getStaker(1)
+      // assert(Number(staker.stake) === 423000)
+    })
+
+    it('should be able to unstake after unstake lock period', async function () {
+      let stakeManager = await StakeManager.deployed()
+      let stateManager = await StateManager.deployed()
+      await stateManager.setEpoch(2)
+      await stakeManager.unstake(2, { 'from': accounts[1] })
+      let staker = await stakeManager.getStaker(1)
+
+      // let staker = await stakeManager.getStaker(1)
+      assert(Number(staker.unstakeAfter) === 0)
+    })
+
+    it('should not be able to withdraw before withdraw lock period', async function () {
+      let stakeManager = await StakeManager.deployed()
+      await assertRevert(stakeManager.withdraw(2, { 'from': accounts[1] }))
+      // let staker = await stakeManager.getStaker(1)
+      // assert(Number(staker.stake) === 423000)
+    })
+
+    it('should not be able to withdraw after withdraw lock period if didnt reveal in last epoch', async function () {
+      let stakeManager = await StakeManager.deployed()
+      let stateManager = await StateManager.deployed()
+      await stateManager.setEpoch(3)
+      await assertRevert(stakeManager.withdraw(3, { 'from': accounts[1] }))
+      // await assertRevert(stakeManager.getStaker(1))
+
+      // let staker = await stakeManager.getStaker(1)
+      // assert(Number(staker.stake) === 0)
     })
   })
 })
--- test/helpers/functions.js
+++ test/helpers/functions.js
@@ -1,4 +1,9 @@
-module.getBiggestStakeAndId = async function (schelling) {
+
+let Web3 = require('web3')
+
+let web3 = new Web3(Web3.givenProvider || 'ws://localhost:8545', null, {})
+
+async function getBiggestStakeAndId (schelling) {
 // async function getBiggestStakeAndId (schelling) {
   let numStakers = await schelling.numStakers()
   let biggestStake = 0
@@ -12,3 +17,44 @@ module.getBiggestStakeAndId = async function (schelling) {
   }
   return ([biggestStake, biggestStakerId])
 }
+
+async function prng (seed, max, blockHashes) {
+  let hashh = await prngHash(seed, blockHashes)
+  let sum = web3.utils.toBN(hashh)
+  max = web3.utils.toBN(max)
+  return (sum.mod(max))
+}
+
+// pseudo random hash generator based on block hashes.
+async function prngHash (seed, blockHashes) {
+// let sum = blockHashes(numBlocks)
+  let sum = await web3.utils.soliditySha3(blockHashes, seed)
+// console.log('prngHash', sum)
+  return (sum)
+}
+
+async function getIteration (schelling, random, biggestStake, stake, stakerId, numStakers, blockHashes) {
+  let j = 0
+  console.log(blockHashes)
+  for (let i = 0; i < 10000000000; i++) {
+// console.log('iteration ', i)
+
+    let isElected = await isElectedProposer(random, i, biggestStake, stake, stakerId, numStakers, blockHashes)
+    if (isElected) return (i)
+  }
+}
+
+async function isElectedProposer (random, iteration, biggestStake, stake, stakerId, numStakers, blockHashes) {
+// rand = 0 -> totalStake-1
+// add +1 since prng returns 0 to max-1 and staker start from 1
+  let seed = await web3.utils.soliditySha3(iteration)
+// console.log('seed', seed)
+  if ((Number(await prng(seed, numStakers, blockHashes)) + 1) !== stakerId) return (false)
+  let seed2 = await web3.utils.soliditySha3(stakerId, iteration)
+  let randHash = await prngHash(seed2, blockHashes)
+  let rand = Number((await web3.utils.toBN(randHash)).mod(await web3.utils.toBN(2 ** 32)))
+// let biggestStake = stakers[biggestStake].stake;
+  if (rand * (biggestStake) > stake * (2 ** 32)) return (false)
+  return (true)
+}
+
