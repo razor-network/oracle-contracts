58d3da364f51e5399c805e4d39dfda8b97c9cf77
Hrishikesh Huilgolkar <hrishikesh911@gmail.com>
2019-12-01 14:26:35 +0700

    redeploy

---
+++ contracts/IDelegator.sol
@@ -0,0 +1,9 @@
+pragma solidity 0.5.10;
+pragma experimental ABIEncoderV2;
+
+
+interface IDelegator {
+    function upgradeDelegate(address newDelegateAddress) external;
+    function getResult(uint256 id) external view returns(uint256);
+    function getJob(uint256 id) external view returns(string memory url, string memory selector, string memory name, bool repeat, uint256 result);
+}
