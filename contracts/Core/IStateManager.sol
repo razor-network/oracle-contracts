70c702f1df0bf75d38ebd0b400d470e928955814
SamAg19 <sameermg1999@gmail.com>
2020-07-28 13:37:29 +0530

    Update contracts and testcases

--- contracts/Core/IStateManager.sol
+++ contracts/Core/IStateManager.sol
@@ -3,13 +3,13 @@ pragma experimental ABIEncoderV2;
 // import "../lib/Structs.sol";
 
 
-abstract contract IStateManager {
+interface IStateManager {
     // for testing only. diable in prod
-    function setEpoch (uint256 epoch) external virtual;
+    function setEpoch (uint256 epoch) external;
 
-    function setState (uint256 state) external virtual;
+    function setState (uint256 state) external;
 
-    function getEpoch () external virtual view returns(uint256);
+    function getEpoch () external view returns(uint256);
 
-    function getState () external virtual view returns(uint256);
+    function getState () external view returns(uint256);
 }
