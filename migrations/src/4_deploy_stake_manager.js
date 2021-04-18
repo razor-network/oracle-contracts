995d53015d6b7632becea21cf30d12f56f10d077
abhishekvispute <abhivispute33@gmail.com>
2021-04-18 23:36:57 +0530

    Constant Configurable Block Rewards by Admin | V1 specific change

--- migrations/src/4_deploy_stake_manager.js
+++ migrations/src/4_deploy_stake_manager.js
@@ -1,7 +1,11 @@
 const { deployContract } = require('../migrationHelpers');
+const { BigNumber } = require('ethers');
+
+// Edit it as per deployment chain
+const blockReward = BigNumber.from(40).mul((BigNumber.from(10).pow(BigNumber.from(18))));
 
 const deployStakeManager = async () => {
-  await deployContract('StakeManager', ['Constants']);
+  await deployContract('StakeManager', ['Constants'], [blockReward.toHexString()]);
 };
 
 module.exports = async () => {
--- test/helpers/constants.js
+++ test/helpers/constants.js
@@ -7,6 +7,7 @@ const EPOCH_LENGTH = BigNumber.from(40);
 const NUM_BLOCKS = 10;
 const NUM_STATES = BigNumber.from(4);
 const STATE_LENGTH = BigNumber.from(10);
+const BLOCK_REWARD = BigNumber.from(40).mul((BigNumber.from(10).pow(BigNumber.from(18))));
 
 module.exports = {
   DEFAULT_ADMIN_ROLE_HASH,
@@ -15,4 +16,5 @@ module.exports = {
   NUM_STATES,
   ONE_ETHER,
   STATE_LENGTH,
+  BLOCK_REWARD
 };
