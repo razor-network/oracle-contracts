f93f99242b02a80faef63cdc91f4b4b8227f5484
Mukul Sharma <54062820+doctoc@users.noreply.github.com>
2021-04-29 20:18:26 +0530

    Added Inactivity Penalty test involving restaking
    Restaking in grace period is not considered an activity. Added a test in which the staker restakes in grace period , but commit/reveals outside grace period . So penalty is still incurred.

--- test/StakeManager.js
+++ test/StakeManager.js
@@ -247,5 +247,45 @@ describe('StakeManager', function () {
       assert(staker.stake.toString() == stake.toString() , 'Stake should have remained the same');
 
     });
+    it('should penalize staker even if they restake and not do commit/reveal in grace_period' , async function(){
+      await mineToNextEpoch();
+      let epoch = await getEpoch();
+      let staker = await stakeManager.getStaker(3);
+      const stake = staker.stake;
+      // current epoch is 36
+      const epochsJumped = GRACE_PERIOD;
+      for(let i = 0;i < epochsJumped ; i++){
+        await mineToNextEpoch();
+      }
+      epoch = await getEpoch();
+      // current epoch is 44 .
+      // epochLastRevealed for staker 3  = 35 ( last test)
+      // Staker 3 was inactive for 44 - 35 - 1 = 8 epochs.
+      const stake2 = BigNumber.from('23000').mul(ONE_ETHER);
+      await schellingCoin.connect(signers[3]).approve(stakeManager.address , stake2);
+      await stakeManager.connect(signers[3]).stake(epoch , stake2);
+      // Staker 3 restakes during grace_period
+      // But epochStaked is not updated , this epoch would still remain be considered as an inactive epoch for staker 3 .
+      // no commit/reveal in this epoch
+
+      await mineToNextEpoch();
+      epoch = await getEpoch();
+      staker = await stakeManager.getStaker(3);
+      const newStake = staker.stake;
+
+      //commit in epoch 45 , outside grace_period
+      const votes = [100, 200, 300, 400, 500, 600, 700, 800, 900];
+      const tree = merkle('keccak256').sync(votes);
+      const root = tree.root();
+      const commitment1 = utils.solidityKeccak256(
+        ['uint256', 'uint256', 'bytes32'],
+        [epoch, root, '0x727d5c9e6d18ed15ce7ac8d3cce6ec8a0e9c02481415c0823ea49d847ccb9ddd']
+      );
+      await voteManager.connect(signers[3]).commit(epoch, commitment1);
+      staker = await stakeManager.getStaker(3);
+      // Total no of inactive epochs = 45 - 35 - 1 = 9
+      // Staker 3 is penalised because total inactive epochs(9) > max allowed inactive epochs i.e grace_period (8)
+      assert(staker.stake.toString() != newStake.toString() , 'Stake should have decreased due to inactivity penalty');
+    });
   });
 });
