91a5172c2e7a65a70b74023b4b73da1fa9ce225d
adi44 <adityadhir97@gmail.com>
2021-05-19 19:16:36 +0530

    removed mint method

--- migrations/src/postDeploymentSetup.js
+++ migrations/src/postDeploymentSetup.js
@@ -1,7 +1,6 @@
 const {
   getdeployedContractInstance,
   readDeploymentFile,
-  readOldDeploymentFile,
 } = require('../migrationHelpers');
 
 const {
@@ -44,21 +43,19 @@ module.exports = async () => {
   // Only transfer tokens in testnets
   if (NETWORK !== 'mainnet') {
     // Add new instance of StakeManager contract & Deployer address as Minter
-    await schellingCoin.addMinter(stakeManagerAddress);
+    const initialSupply = await schellingCoin.INITIAL_SUPPLY();
     await schellingCoin.addMinter(signers[0].address);
+    await schellingCoin.mint(stakeManagerAddress, initialSupply);
 
     if (SCHELLING_COIN_ADDRESS !== '') {
-      const { StakeManager: oldStakeManagerAddress } = await readOldDeploymentFile();
-
       // if previous instances of Schelling Coin is reused again and again,
       // then initial balance will get depleted, thus intial tokens minting is needed,
       // each time Schelling Coin instance is reused
       const initialSupply = await schellingCoin.INITIAL_SUPPLY();
       await schellingCoin.mint(signers[0].address, initialSupply);
+      await schellingCoin.mint(stakeManagerAddress, initialSupply);
 
       // Remove previous instance of StakeManager contract & Deployer address from Minter
-      await schellingCoin.removeMinter(oldStakeManagerAddress);
-      await schellingCoin.removeMinter(signers[0].address);
     }
 
     for (let i = 0; i < stakerAddressList.length; i++) {
