808f9289d563a3ac3c3e5306684b4963c5e3f106
Skanda Bhat <bhat.skanda.m@gmail.com>
2021-07-23 18:51:20 +0530

    Hardhat upgrade and solidity downgrade (#228)
    * Change version to 0.8.4
    
    * Update hardhat version to latest release
    
    https://github.com/nomiclabs/hardhat/releases/tag/hardhat-core-v2.5.0
    
    * Cleanup
    
    * modify revert message

--- hardhat.config.js
+++ hardhat.config.js
@@ -32,7 +32,7 @@ const ENV_CHAIN_IDS = {
 module.exports = {
   defaultNetwork: 'hardhat',
   solidity: {
-    version: '0.8.6',
+    version: '0.8.4',
     settings: {
       optimizer: {
         enabled: true,
