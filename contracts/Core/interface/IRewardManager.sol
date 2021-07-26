9f736d56ff7169b5f3de33cbf296478a71c8d114
hrishikeshio <hrishikesh911@gmail.com>
2021-07-27 00:40:06 +0400

    prettify code to pass the build

--- contracts/Core/interface/IRewardManager.sol
+++ contracts/Core/interface/IRewardManager.sol
@@ -3,9 +3,8 @@ pragma solidity ^0.8.0;
 
 import "../../lib/Structs.sol";
 
-
 interface IRewardManager {
+    function givePenalties(uint256 stakerId, uint256 epoch) external;
 
-    function givePenalties (uint256 stakerId, uint256 epoch) external;
     function giveBlockReward(uint256 stakerId, uint256 epoch) external;
 }
--- contracts/Core/storage/AssetStorage.sol
+++ contracts/Core/storage/AssetStorage.sol
@@ -3,12 +3,13 @@ pragma solidity ^0.8.0;
 
 import "../../lib/Structs.sol";
 
-
 contract AssetStorage {
-    mapping (uint256 => Structs.Job) public jobs;
-    mapping (uint256 => Structs.Collection) public collections;
-
-    enum assetTypes { None, Job, Collection }
-
+    enum assetTypes {
+        None,
+        Job,
+        Collection
+    }
+    mapping(uint256 => Structs.Job) public jobs;
+    mapping(uint256 => Structs.Collection) public collections;
     uint256 public numAssets;
 }
