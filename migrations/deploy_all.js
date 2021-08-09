cada3eb2d753198cf8b26a6a9764a1fc2573838b
abhishekvispute <abhivispute33@gmail.com>
2021-08-09 14:42:27 +0530

    faucet moved to seprate repo : razor-network/faucet

--- migrations/deploy_all.js
+++ migrations/deploy_all.js
@@ -7,7 +7,7 @@ const stakeManagerMigration = require('./src/5_deploy_stake_manager');
 const rewardManagerMigration = require('./src/6_deploy_reward_manager');
 const voteManagerMigration = require('./src/7_deploy_vote_manager');
 const delegatorMigration = require('./src/8_deploy_delegator');
-const RAZORAndFacuetMigration = require('./src/9_deploy_razor');
+const RAZORMigration = require('./src/9_deploy_razor');
 const postDeploymentSetup = require('./src/postDeploymentSetup');
 
 async function main() {
@@ -20,7 +20,7 @@ async function main() {
   await rewardManagerMigration();
   await voteManagerMigration();
   await delegatorMigration();
-  await RAZORAndFacuetMigration();
+  await RAZORMigration();
   await postDeploymentSetup();
 }
 
