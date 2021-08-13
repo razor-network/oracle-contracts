975a5d99cb074f815d9e629de5a87ac84c8d2e87
Aditya Dhir <31381639+adi44@users.noreply.github.com>
2021-08-13 13:57:01 +0530

    Fixed testcase

--- test/BlockManager.js
+++ test/BlockManager.js
@@ -516,6 +516,31 @@ describe('BlockManager', function () {
       assertBNEqual(dispute.lastVisitedStaker, sortedStakers[sortedStakers.length - 1], 'lastVisited should match');
       await blockManager.connect(signers[19]).finalizeDispute(epoch, 0);
     });
+    it('staker should not be able to propose when not elected', async function () {
+      await mineToNextEpoch();
+      const epoch = await getEpoch();
+      const stakerIdAcc8 = await stakeManager.stakerIds(signers[8].address);
+      const staker = await stakeManager.getStaker(stakerIdAcc8);
+      const { biggestInfluencerId } = await getBiggestInfluenceAndId(stakeManager);
+      const iteration = await getIteration(voteManager, stakeManager, staker);
+      const votes = [100, 200, 300, 400, 500, 600, 700, 800, 900];
+
+      const commitment1 = utils.solidityKeccak256(
+        ['uint32', 'uint48[]', 'bytes32'],
+        [epoch, votes, '0x727d5c9e6d18ed15ce7ac8d3cce6ec8a0e9c02481415c0823ea49d847ccb9ddd']
+      );
+      await voteManager.connect(signers[8]).commit(epoch, commitment1);
+      await mineToNextState();
+      await voteManager.connect(signers[8]).reveal(epoch, votes,
+        '0x727d5c9e6d18ed15ce7ac8d3cce6ec8a0e9c02481415c0823ea49d847ccb9ddd');
+      await mineToNextState();
+
+      const tx = blockManager.connect(signers[8]).propose(epoch,
+        [100, 200, 300, 400, 500, 600, 700, 800, 900],
+        iteration,
+        biggestInfluencerId);
+      assertRevert(tx, 'not elected');
+    });
     it('staker should not be able to propose when not not revealed', async function () {
       await mineToNextEpoch();
       const epoch = await getEpoch();
@@ -534,30 +559,6 @@ describe('BlockManager', function () {
       await mineToNextState();
 
       const tx = blockManager.connect(signers[8]).propose(epoch,
-        [100, 200, 300, 400, 500, 600, 700, 800, 900],
-        iteration,
-        biggestInfluencerId);
-      assertRevert(tx, 'Cannot propose without revealing');
-    });
-    it('staker should not be able to propose when not stake below minStake', async function () {
-      await mineToNextEpoch();
-      const epoch = await getEpoch();
-      const stakerIdAcc2 = await stakeManager.stakerIds(signers[6].address);
-      const staker = await stakeManager.getStaker(stakerIdAcc2);
-      const { biggestInfluencerId } = await getBiggestInfluenceAndId(stakeManager);
-      const iteration = await getIteration(voteManager, stakeManager, staker);
-      const votes = [100, 200, 300, 400, 500, 600, 700, 800, 900];
-
-      const commitment1 = utils.solidityKeccak256(
-        ['uint32', 'uint48[]', 'bytes32'],
-        [epoch, votes, '0x727d5c9e6d18ed15ce7ac8d3cce6ec8a0e9c02481415c0823ea49d847ccb9ddd']
-      );
-      await voteManager.connect(signers[6]).commit(epoch, commitment1);
-      await mineToNextState();
-      await voteManager.connect(signers[6]).reveal(epoch, votes,
-        '0x727d5c9e6d18ed15ce7ac8d3cce6ec8a0e9c02481415c0823ea49d847ccb9ddd');
-      await mineToNextState();
-      const tx = blockManager.connect(signers[6]).propose(epoch,
         [100, 200, 300, 400, 500, 600, 700, 800, 900],
         iteration,
         biggestInfluencerId);
