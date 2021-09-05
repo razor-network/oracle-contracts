1477f5b9473ebac0e61d75ff3b0734b035b2dd5a
Aditya Dhir <aditya@razor.network>
2021-09-05 19:31:57 +0530

    gasCompare

---
+++ scripts/gasCompare.js
@@ -0,0 +1,62 @@
+const fs = require("fs");
+let arguments = process.argv
+
+//method to read the file information
+
+let getFileData = (filePath) => {
+
+    try{
+        const fileData = fs.readFileSync(filePath);
+        const response = JSON.parse(fileData)
+        return response.info.methods;
+
+    } catch (err) {
+        console.log(err);
+        return
+    }  
+
+}
+//method to calculate median value
+
+let calculateMedianValue = (numbers) => {
+    const sorted = numbers.slice().sort((a, b) => a - b);
+    const middle = Math.floor(sorted.length / 2);
+
+    if (sorted.length % 2 === 0) {
+        return (sorted[middle - 1] + sorted[middle]) / 2;
+    }
+
+    return sorted[middle];
+
+}
+//method to compare gas values before and after.
+
+let compareValue = (a,b) => {
+
+    if(a<b){
+        return ((a-b)*100)/a;
+    }
+    else if(b<a){
+        return ((a-b)*100)/a ;
+    }
+    else{
+        return 0
+    }      
+}
+// method to compare the gas Consumption.
+
+let gasCompare = () => {
+    let list = {}
+    const gasDataI = getFileData(arguments[2]);
+    const gasDataII = getFileData(arguments[3]);
+    for(i in gasDataI){
+        if(i in gasDataII){
+        if(calculateMedianValue(gasDataI[i].gasData) && compareValue(calculateMedianValue(gasDataI[i].gasData),calculateMedianValue(gasDataII[i].gasData))!=0)
+        list[gasDataI[i].method] = compareValue(calculateMedianValue(gasDataI[i].gasData),calculateMedianValue(gasDataII[i].gasData))
+
+    }
+}
+console.log(list)
+}
+
+gasCompare();
