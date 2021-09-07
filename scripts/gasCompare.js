2ca5bd0cd802309af887af4110d8a9f478f88444
Aditya Dhir <aditya@razor.network>
2021-09-07 15:10:09 +0530

    markdown

--- scripts/gasCompare.js
+++ scripts/gasCompare.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
-const Commenter = require('../node_modules/circleci-pr-commenter/dist/index')
+const markdown = require('json-to-markdown-table');
+const Commenter = require('../node_modules/circleci-pr-commenter/dist/index');
 const commenter = new Commenter()
 
 let arguments = process.argv
@@ -47,18 +48,26 @@ let compareValue = (a,b) => {
 // method to compare the gas Consumption.
 
 let gasCompare = async () => {
-    let list = {}
+    let coloumn = ['contract','method','change%'];
+    let gasChangeData = [];
     const gasDataI = getFileData(arguments[2]);
     const gasDataII = getFileData(arguments[3]);
     for(i in gasDataI){
         if(i in gasDataII){
         if(calculateMedianValue(gasDataI[i].gasData) && compareValue(calculateMedianValue(gasDataI[i].gasData),calculateMedianValue(gasDataII[i].gasData))!=0)
-        list[gasDataI[i].method] = compareValue(calculateMedianValue(gasDataI[i].gasData),calculateMedianValue(gasDataII[i].gasData))
+        {
+            let obj = {'contract': gasDataI[i].contract, 
+            'method':gasDataI[i].method, 
+            'change%': compareValue(calculateMedianValue(gasDataI[i].gasData),calculateMedianValue(gasDataII[i].gasData)) };
+            gasChangeData.push(obj);
+        }
+        
 
     }
 }
-if(Object.keys(list).length!==0){
-    await commenter.createOrUpdateComment('gasCompare', `${JSON.stringify(list)} following are the changes`).catch(err=>{
+let markdownstring = markdown(gasChangeData,coloumn);
+if(gasChangeData.length!==0){
+    await commenter.createOrUpdateComment('gasCompare', markdownstring ).catch(err=>{
         console.log(err);
     })
 }
