7ed1e42915838c0bd3d3ce15984e8261ecb99935
Aditya Dhir <aditya@razor.network>
2021-09-15 15:06:10 +0530

    Lint Fixes

--- contracts/Core/interface/IBlockManager.sol
+++ contracts/Core/interface/IBlockManager.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.0;
 import "../../lib/Structs.sol";
 
 interface IBlockManager {
-
     function confirmPreviousEpochBlock(uint32 stakerId) external;
 
     function getBlock(uint32 epoch) external view returns (Structs.Block memory _block);
