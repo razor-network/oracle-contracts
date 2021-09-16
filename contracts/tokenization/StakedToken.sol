0e9fa47713e077c70876d1503af2bc2d94883a0b
Aditya Dhir <aditya@razor.network>
2021-09-16 11:51:11 +0530

    imported interface

--- contracts/tokenization/StakedToken.sol
+++ contracts/tokenization/StakedToken.sol
@@ -3,10 +3,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "./IStakedToken.sol";
-
-interface IStakeManager {
-    function getStake(uint32 stakerId) external view returns (uint256);
-}
+import "../Core/interface/IStakeManager.sol";
 
 contract StakedToken is ERC20, IStakedToken {
     address private _owner;
