182aace28d0208bb239e3e5e7d5c11f331c9faf5
SamAg19 <sameermg1999@gmail.com>
2021-10-12 16:09:34 +0530

    slither issue fixed

--- contracts/Delegator.sol
+++ contracts/Delegator.sol
@@ -3,11 +3,12 @@ pragma solidity ^0.8.0;
 
 import "./Core/interface/IAssetManager.sol";
 import "./Core/interface/IBlockManager.sol";
+import "./IDelegator.sol";
 import "./Core/interface/IParameters.sol";
 import "./Core/storage/Constants.sol";
 import "./Core/ACL.sol";
 
-contract Delegator is ACL, Constants {
+contract Delegator is ACL, Constants, IDelegator {
     mapping(bytes32 => uint8) public ids;
 
     IParameters public parameters;
@@ -18,7 +19,7 @@ contract Delegator is ACL, Constants {
         address newDelegateAddress,
         address newResultAddress,
         address parametersAddress
-    ) external onlyRole(DEFAULT_ADMIN_ROLE) {
+    ) external override onlyRole(DEFAULT_ADMIN_ROLE) {
         require(newDelegateAddress != address(0x0), "Zero Address check");
         require(newResultAddress != address(0x0), "Zero Address check");
         require(parametersAddress != address(0x0), "Zero Address check");
@@ -27,21 +28,21 @@ contract Delegator is ACL, Constants {
         parameters = IParameters(parametersAddress);
     }
 
-    function setIDName(string calldata name, uint8 _id) external onlyRole(DELEGATOR_MODIFIER_ROLE) {
+    function setIDName(string calldata name, uint8 _id) external override onlyRole(DELEGATOR_MODIFIER_ROLE) {
         bytes32 _name = keccak256(abi.encodePacked(name));
         require(ids[_name] == 0, "Similar collection exists");
         ids[_name] = _id;
     }
 
-    function getNumActiveAssets() external view returns (uint256) {
+    function getNumActiveAssets() external override view returns (uint256) {
         return assetManager.getNumActiveAssets();
     }
 
-    function getActiveAssets() external view returns (uint8[] memory) {
+    function getActiveAssets() external override view returns (uint8[] memory) {
         return assetManager.getActiveAssets();
     }
 
-    function getResult(bytes32 _name) external view returns (uint32, int8) {
+    function getResult(bytes32 _name) external override view returns (uint32, int8) {
         uint8 index = assetManager.getAssetIndex(ids[_name]);
         uint32 epoch = parameters.getEpoch();
         uint32[] memory medians = blockManager.getBlock(epoch - 1).medians;
