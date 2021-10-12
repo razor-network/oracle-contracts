c920410a5224ba0988209b44f9b319ea66b8445b
SamAg19 <sameermg1999@gmail.com>
2021-10-12 16:16:08 +0530

    linting issue fixed

--- contracts/Delegator.sol
+++ contracts/Delegator.sol
@@ -34,15 +34,15 @@ contract Delegator is ACL, Constants, IDelegator {
         ids[_name] = _id;
     }
 
-    function getNumActiveAssets() external override view returns (uint256) {
+    function getNumActiveAssets() external view override returns (uint256) {
         return assetManager.getNumActiveAssets();
     }
 
-    function getActiveAssets() external override view returns (uint8[] memory) {
+    function getActiveAssets() external view override returns (uint8[] memory) {
         return assetManager.getActiveAssets();
     }
 
-    function getResult(bytes32 _name) external override view returns (uint32, int8) {
+    function getResult(bytes32 _name) external view override returns (uint32, int8) {
         uint8 index = assetManager.getAssetIndex(ids[_name]);
         uint32 epoch = parameters.getEpoch();
         uint32[] memory medians = blockManager.getBlock(epoch - 1).medians;
