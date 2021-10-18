bf7af6f58c020218997de0e2b793d4d7bf0f316c
Abhishek Vispute <abhivispute33@gmail.com>
2021-10-18 19:24:05 +0530

    slither warning ignored

--- contracts/lib/Random.sol
+++ contracts/lib/Random.sol
@@ -3,12 +3,16 @@ pragma solidity ^0.8.0;
 
 library Random {
     // pseudo random number generator based on hash. returns 0 -> max-1
+    // slither ignore reason : Internal library
+    // slither-disable-next-line dead-code
     function prng(uint256 max, bytes32 randHash) internal pure returns (uint256) {
         uint256 sum = uint256(randHash);
         return (sum % max);
     }
 
     // pseudo random hash generator based on hashes.
+    // slither ignore reason : Internal library
+    // slither-disable-next-line dead-code
     function prngHash(bytes32 seed, bytes32 salt) internal pure returns (bytes32) {
         bytes32 prngHashVal = keccak256(abi.encodePacked(seed, salt));
         return (prngHashVal);
