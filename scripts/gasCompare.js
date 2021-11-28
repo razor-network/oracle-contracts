c8f381dd379bb2eaa611f8f28fb4a09219f99659
Aditya Dhir <aditya@razor.network>
2021-11-28 11:26:20 +0530

    added minimum

--- scripts/gasCompare.js
+++ scripts/gasCompare.js
@@ -1,7 +1,12 @@
 const fs = require("fs");
 const markdown = require('json-to-markdown-table');
 const Commenter = require('circleci-pr-commenter');
-const commenter = new Commenter()
+try{
+    const commenter = new Commenter()
+}
+catch (err){
+    console.log("Script is called in dev environment")
+};
 
 let arguments = process.argv
 
@@ -28,6 +33,14 @@ let calculateMaximumValue = (numbers) => {
     return numbers[numbers.length-1];
 
 }
+let calculateMinimumValue = (numbers) => {
+    if(numbers.length===0){
+        return 0
+    }
+
+    return numbers[0];
+
+}
 //method to compare gas values before and after.
 
 let compareValue = (gas_usage_current,gas_usage_master) => {
@@ -43,22 +56,28 @@ let compareValue = (gas_usage_current,gas_usage_master) => {
 // method to compare the gas Consumption.
 
 let gasCompare = async () => {
-    let coloumn = ['Contract','Method','Change%','Current','Master','Diff'];
+    let coloumn = ['Contract','Method','Change%(Maximum)','Current(Maximum)','Master(Maximum)','Diff','Current(Minimum)','Master(Maximum)','Diff','Change%(Minimum)'];
     let gasChangeData = [];
     const gasDataI = getFileData(arguments[2]);
     const gasDataII = getFileData(arguments[3]);
     for(i in gasDataI){
         if(i in gasDataII){        
-            let change = compareValue(calculateMaximumValue(gasDataI[i].gasData),calculateMaximumValue(gasDataII[i].gasData));  
-            let diff = calculateMaximumValue(gasDataI[i].gasData)-calculateMaximumValue(gasDataII[i].gasData);
+            let changeMaximum = compareValue(calculateMaximumValue(gasDataI[i].gasData),calculateMaximumValue(gasDataII[i].gasData));  
+            let changeMinimum = compareValue(calculateMinimumValue(gasDataI[i].gasData),calculateMinimumValue(gasDataII[i].gasData)); 
+            let diffMaximum = calculateMaximumValue(gasDataI[i].gasData)-calculateMaximumValue(gasDataII[i].gasData);
+            let diffMinimum = calculateMinimumValue(gasDataI[i].gasData)-calculateMinimumValue(gasDataII[i].gasData);
             if(calculateMaximumValue(gasDataI[i].gasData) && change!=0)
             {
                 let obj = {'Contract': gasDataI[i].contract, 
                 'Method':gasDataI[i].method, 
-                'Current':calculateMaximumValue(gasDataI[i].gasData),
-                'Master' :calculateMaximumValue(gasDataII[i].gasData),
-                'Change%': change > 0 ? '+' + change.toFixed(2).toString() :'-' + Math.abs(change.toFixed(2)),
-                'Diff' : diff > 0 ? '+' + diff.toFixed(2).toString() :'-' + Math.abs(diff.toFixed(2))
+                'Current(Maxiumum)':calculateMaximumValue(gasDataI[i].gasData),
+                'Master(Maximum)' :calculateMaximumValue(gasDataII[i].gasData),
+                'Change%(Maximum)': changeMaximum > 0 ? '+' + change.toFixed(2).toString() :'-' + Math.abs(change.toFixed(2)),
+                'Diff(Maximum)' : diffMaximum > 0 ? '+' + diffMaximum.toFixed(2).toString() :'-' + Math.abs(diffMaximum.toFixed(2)),
+                'Current(Minimum)':calculateMinimumValue(gasDataI[i].gasData),
+                'Master(Minimum)' :calculateMinimumValue(gasDataII[i].gasData),
+                'Diff(Maximum)' : diffMinimum > 0 ? '+' + diffMinimum.toFixed(2).toString() :'-' + Math.abs(diffMinimum.toFixed(2)),
+                'Change%(Minimum)':changeMinimum > 0 ? '+' + change.toFixed(2).toString() :'-' + Math.abs(change.toFixed(2)),
                 }
                 gasChangeData.push(obj);
             }
@@ -66,14 +85,21 @@ let gasCompare = async () => {
     }
 let markdownstring = markdown(gasChangeData,coloumn);
 if(gasChangeData.length!==0){
-    await commenter.createOrUpdateComment('gasCompare', markdownstring ).catch(err=>{
-        console.log(err);
-    })
+    try{
+    await commenter.createOrUpdateComment('gasCompare', markdownstring )
+    }
+    catch(err){
+        console.log(markdownstring);
+    }
 }
 else{
-    await commenter.createOrUpdateComment('gasCompare', `No changes found in gas Consumption`).catch(err=>{
-        console.log(err);
-    })
+    try{
+        await commenter.createOrUpdateComment('gasCompare', `No changes found in gas Consumption`)
+    }
+    catch(err){
+        console.log(`No changes found in gas Consumption`);
+    }
+        
 }
 }
 
