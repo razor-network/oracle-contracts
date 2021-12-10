29b294f34bc8993b0e6ca7473818de280fbf8281
SamAg19 <52957842+SamAg19@users.noreply.github.com>
2021-12-10 12:45:54 +0530

    Staker wont be able to jump queue anymore. (#602)
    * changes to biggestInfluence in block proposals
    
    * Revert "changes to biggestInfluence in block proposals"
    
    This reverts commit b0ed069c2913f56b11eb3b6ec1026c2c71e1cbea.
    
    * influence order changes in insertAppropriately
    
    * linting fixes
    
    * removed dispute for biggest influence
    
    * Dispute readded
    
    * should insert blockId instead of 0 when length is 0
    
    * Revert "should insert blockId instead of 0 when length is 0"
    
    This reverts commit 7fe469e9205f6446863d0978649091f070b5634e.
    
    * numProposedBlocks increments on insertion of block
    
    * optimization done

--- contracts/Core/BlockManager.sol
+++ contracts/Core/BlockManager.sol
@@ -66,9 +66,11 @@ contract BlockManager is Initializable, BlockStorage, StateManager, BlockManager
         uint256 biggestInfluence = voteManager.getInfluenceSnapshot(epoch, biggestInfluencerId);
         if (sortedProposedBlockIds[epoch].length == 0) numProposedBlocks = 0;
         proposedBlocks[epoch][numProposedBlocks] = Structs.Block(true, proposerId, medians, iteration, biggestInfluence);
-        _insertAppropriately(epoch, numProposedBlocks, iteration, biggestInfluence);
+        bool isAdded = _insertAppropriately(epoch, numProposedBlocks, iteration, biggestInfluence);
         epochLastProposed[proposerId] = epoch;
-        numProposedBlocks = numProposedBlocks + 1;
+        if (isAdded) {
+            numProposedBlocks = numProposedBlocks + 1;
+        }
         emit Proposed(epoch, proposerId, medians, iteration, biggestInfluencerId, block.timestamp);
     }
 
@@ -215,23 +217,30 @@ contract BlockManager is Initializable, BlockStorage, StateManager, BlockManager
         uint32 blockId,
         uint256 iteration,
         uint256 biggestInfluence
-    ) internal {
+    ) internal returns (bool isAdded) {
         uint8 sortedProposedBlockslength = uint8(sortedProposedBlockIds[epoch].length);
 
         if (sortedProposedBlockslength == 0) {
             sortedProposedBlockIds[epoch].push(0);
             blockIndexToBeConfirmed = 0;
-            return;
+            return true;
+        }
+
+        if (proposedBlocks[epoch][sortedProposedBlockIds[epoch][0]].biggestInfluence > biggestInfluence) {
+            return false;
+        }
+
+        if (proposedBlocks[epoch][sortedProposedBlockIds[epoch][0]].biggestInfluence < biggestInfluence) {
+            for (uint8 i = 0; i < sortedProposedBlockslength; i++) {
+                sortedProposedBlockIds[epoch].pop();
+            }
+            sortedProposedBlockIds[epoch].push(blockId);
+            return true;
         }
 
         for (uint8 i = 0; i < sortedProposedBlockslength; i++) {
-            // Replace : New Block has better biggest influence
-            if (proposedBlocks[epoch][sortedProposedBlockIds[epoch][i]].biggestInfluence < biggestInfluence) {
-                sortedProposedBlockIds[epoch][i] = blockId;
-                return;
-            }
             // Push and Shift
-            else if (proposedBlocks[epoch][sortedProposedBlockIds[epoch][i]].iteration > iteration) {
+            if (proposedBlocks[epoch][sortedProposedBlockIds[epoch][i]].iteration > iteration) {
                 sortedProposedBlockIds[epoch].push(blockId);
 
                 sortedProposedBlockslength = sortedProposedBlockslength + 1;
@@ -246,12 +255,13 @@ contract BlockManager is Initializable, BlockStorage, StateManager, BlockManager
                     sortedProposedBlockIds[epoch].pop();
                 }
 
-                return;
+                return true;
             }
         }
         // Worst Iteration and for all other blocks, influence was >=
         if (sortedProposedBlockIds[epoch].length < maxAltBlocks) {
             sortedProposedBlockIds[epoch].push(blockId);
+            return true;
         }
     }
 
