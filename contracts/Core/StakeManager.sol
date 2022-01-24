4f259e2ef57123cbdbc800008aa7edd8a80ca9f3
SamAg19 <sameermg1999@gmail.com>
2022-01-24 19:15:59 +0530

    patch fix

--- contracts/Core/StakeManager.sol
+++ contracts/Core/StakeManager.sol
@@ -172,10 +172,7 @@ contract StakeManager is Initializable, StakeStorage, StateManager, Pause, Stake
     // If this period pass, lock expires and she will have to extendLock() to able to withdraw again
     /// @param stakerId The Id of staker associated with sRZR which user want to unstake
     /// @param sAmount The Amount in sRZR
-    function unstake(uint32 stakerId, uint256 sAmount) external initialized whenNotPaused {
-        State currentState = _getState(epochLength);
-        require(currentState != State.Propose, "Unstake: NA Propose");
-        require(currentState != State.Dispute, "Unstake: NA Dispute");
+    function unstake(uint32 stakerId, uint256 sAmount) external initialized whenNotPaused checkState(State.Confirm, epochLength) {
         uint32 epoch = _getEpoch(epochLength);
 
         Structs.Staker storage staker = stakers[stakerId];
