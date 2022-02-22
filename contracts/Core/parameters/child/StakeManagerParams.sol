9719c9c5606040e562f2d9fc70ab6e7a113570ed
GauravJain9 <gauravsethia91@gmail.com>
2022-02-22 21:52:10 +0530

    Increased coverage on RewardManager and stakeManagerParams

--- contracts/Core/parameters/child/StakeManagerParams.sol
+++ contracts/Core/parameters/child/StakeManagerParams.sol
@@ -31,7 +31,7 @@ abstract contract StakeManagerParams is ACL, IStakeManagerParams, Constants {
         uint32 _burn,
         uint32 _keep
     ) external override onlyRole(GOVERNANCE_ROLE) {
-        require(_bounty + _burn + _keep <= BASE_DENOMINATOR, "Slash nums addtion exceeds 10000");
+        require(_bounty + _burn + _keep <= BASE_DENOMINATOR, "Slash nums addtion exceeds 10000000");
         // slither-disable-next-line events-maths
         slashNums = SlashNums(_bounty, _burn, _keep);
     }
