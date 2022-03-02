8539f6c3f75fa7954df4052ea03514c828e41b08
SamAg19 <52957842+SamAg19@users.noreply.github.com>
2022-03-02 12:23:13 +0530

    docs: natspec added to delegator, block manager and vote manager (#729)
    * docs: natspec added to delegator, block manager and vote manager
    
    * docs: natspec fix
    
    * docs: natspec added to events
    
    * docs: natspec fix
    
    * docs: lint fix

--- contracts/Core/storage/BlockStorage.sol
+++ contracts/Core/storage/BlockStorage.sol
@@ -3,19 +3,21 @@ pragma solidity ^0.8.0;
 import "../../lib/Structs.sol";
 
 contract BlockStorage {
-    //epoch -> address -> dispute
+    /// @notice mapping of epoch -> address -> dispute
     mapping(uint32 => mapping(address => Structs.Dispute)) public disputes;
-    //epoch -> blockId -> block
+    /// @notice mapping of epoch -> blockId -> block
     mapping(uint32 => mapping(uint32 => Structs.Block)) public proposedBlocks;
-    //epoch->blockId
+    /// @notice mapping of epoch->blockId
     mapping(uint32 => uint32[]) public sortedProposedBlockIds;
-    //stakerId->epoch
+    /// @notice mapping of stakerId->epoch
     mapping(uint32 => uint32) public epochLastProposed;
 
+    /// @notice total number of proposed blocks in an epoch
     // slither-disable-next-line constable-states
     uint32 public numProposedBlocks;
+    /// @notice block index that is to be confirmed if not disputed
     // slither-disable-next-line constable-states
     int8 public blockIndexToBeConfirmed; // Index in sortedProposedBlockIds
-    // epoch -> blocks
+    /// @notice mapping of  epoch -> blocks
     mapping(uint32 => Structs.Block) public blocks;
 }
