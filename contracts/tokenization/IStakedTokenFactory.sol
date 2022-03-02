562aba8fc48ded9ba41c379e3ffbac56b828e80a
SamAg19 <52957842+SamAg19@users.noreply.github.com>
2022-03-02 12:24:21 +0530

    docs: natspec added to staked token contracts (#734)

--- contracts/tokenization/IStakedTokenFactory.sol
+++ contracts/tokenization/IStakedTokenFactory.sol
@@ -2,5 +2,10 @@
 pragma solidity ^0.8.0;
 
 interface IStakedTokenFactory {
+    /**
+     * @dev a factory contract where the sRZR for a new staker is being deployed
+     * @param stakeManagerAddress address of the stake Manager contract
+     * @param stakedID id of the staker whom the sRZR is being deployed
+     */
     function createStakedToken(address stakeManagerAddress, uint32 stakedID) external returns (address);
 }
--- contracts/tokenization/StakedTokenFactory.sol
+++ contracts/tokenization/StakedTokenFactory.sol
@@ -5,6 +5,7 @@ import "./StakedToken.sol";
 import "./IStakedTokenFactory.sol";
 
 contract StakedTokenFactory is IStakedTokenFactory {
+    /// @inheritdoc IStakedTokenFactory
     function createStakedToken(address stakeManagerAddress, uint32 stakerID) external override returns (address) {
         require(stakeManagerAddress != address(0x0), "zero address check");
         StakedToken sToken = new StakedToken(stakeManagerAddress, stakerID);
