f0da8e84da8d5711ea3a26eabb36d741920cea82
Aditya Dhir <aditya@razor.network>
2022-03-14 16:21:22 +0530

    feat: fixed slither warnings and corrected postdeployment script

--- contracts/Core/StateManager.sol
+++ contracts/Core/StateManager.sol
@@ -12,7 +12,7 @@ contract StateManager is Constants {
      * @notice a check to ensure the epoch value sent in the function is of the currect epoch
      */
     modifier checkEpoch(uint32 epoch) {
-        // slither-disable-next-line incorrect-equality
+        // slither-disable-next-line incorrect-equality, block-timestamp
         require(epoch == _getEpoch(), "incorrect epoch");
         _;
     }
@@ -21,7 +21,7 @@ contract StateManager is Constants {
      * @notice a check to ensure the function was called in the state specified
      */
     modifier checkState(State state, uint8 buffer) {
-        // slither-disable-next-line incorrect-equality
+        // slither-disable-next-line incorrect-equality, block-timestamp
         require(state == _getState(buffer), "incorrect state");
         _;
     }
@@ -30,7 +30,7 @@ contract StateManager is Constants {
      * @notice a check to ensure the function was not called in the state specified
      */
     modifier notState(State state, uint8 buffer) {
-        // slither-disable-next-line incorrect-equality
+        // slither-disable-next-line incorrect-equality, block-timestamp
         require(state != _getState(buffer), "incorrect state");
         _;
     }
@@ -43,23 +43,26 @@ contract StateManager is Constants {
         uint32 epoch,
         uint8 buffer
     ) {
-        // slither-disable-next-line incorrect-equality
+        // slither-disable-next-line incorrect-equality, block-timestamp
         require(epoch == _getEpoch(), "incorrect epoch");
-        // slither-disable-next-line incorrect-equality
+        // slither-disable-next-line incorrect-equality, block-timestamp
         require(state == _getState(buffer), "incorrect state");
         _;
     }
 
     function _getEpoch() internal view returns (uint32) {
+        // slither-disable-next-line block-timestamp
         return (uint32(block.timestamp) / (EPOCH_LENGTH));
     }
 
     function _getState(uint8 buffer) internal view returns (State) {
         uint8 lowerLimit = buffer;
         uint8 upperLimit = uint8(EPOCH_LENGTH / NUM_STATES) - buffer;
+        // slither-disable-next-line block-timestamp
         if (block.timestamp % (EPOCH_LENGTH / NUM_STATES) > upperLimit || block.timestamp % (EPOCH_LENGTH / NUM_STATES) < lowerLimit) {
             return State.Buffer;
         }
+        // slither-disable-next-line block-timestamp
         uint8 state = uint8(((block.timestamp) / (EPOCH_LENGTH / NUM_STATES)) % (NUM_STATES));
         return State(state);
     }
