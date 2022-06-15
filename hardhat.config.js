cbd4a85b10e99a7c6b2a36226090e551237d9114
yohanelly95 <yohan.nelson@gmail.com>
2022-06-15 16:42:42 +0530

    test: use post deployment helpers in postdeployment test

--- hardhat.config.js
+++ hardhat.config.js
@@ -90,6 +90,9 @@ module.exports = {
       accounts: { mnemonic: MNEMONIC },
       chainId: ENV_CHAIN_IDS[NETWORK],
     },
+    hardhat: {
+      saveDeployments: false,
+    },
   },
   etherscan: {
     apiKey: ETHERSCAN_KEY,
