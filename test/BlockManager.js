a11694be699b62fcfc5d66fa73452d1bfd738c95
yohanelly95 <yohan.nelson@gmail.com>
2022-07-07 12:17:37 +0530

    test: fix blockM test getSecret

--- test/BlockManager.js
+++ test/BlockManager.js
@@ -1681,9 +1681,9 @@ describe('BlockManager', function () {
 
       await mineToNextEpoch();
       const epoch = await getEpoch();
-      const secret = '0x727d5c9e6d18ed15ce7ac8dececece8a0e9418555555c08bceedbcede56d8bc9';
+      let secret = await getSecret(signers[17]);
       await commit(signers[19], 0, voteManager, collectionManager, secret, blockManager);
-
+      secret = await getSecret(signers[19]);
       await mineToNextState();
       await reveal(signers[19], 0, voteManager, stakeManager);
 
